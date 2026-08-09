0548600d425c33af010ce28ee0cb9d6370289651
TDK-99 <vale.99.mezzo@gmail.com>
2026-08-10 01:23:42 +0200

    modify: semplify class for the router output

--- src/src/schemas.py
+++ src/src/schemas.py
@@ -3,13 +3,9 @@ from enum import Enum
 
 # agent router schema
 
-class IntentType(BaseModel): #schema for the intent
+class UserIntent(BaseModel):
     info: bool = Field(description="L'utente chiede info su tasse o stipendio")
     data: bool = Field(description="L'utente fornisce dati per il calcolo")
     update: bool = Field(description="L'utente vuole modificare dati già inseriti")
     off_topic: bool = Field(description="L'utente parla di altro")
-
-
-class UserIntent(BaseModel): #output from the router
-    intent: IntentType
     message: str
