a842cb8f3cc1d4c92061c7505f9568e1b0be7a76
TDK-99 <vale.99.mezzo@gmail.com>
2026-08-10 01:17:13 +0200

    modify: insert the async def of the router agent

---
+++ src/src/router_agent.py
@@ -0,0 +1,39 @@
+from agents import Agent, Runner, trace
+
+from config import  oss_model
+from schemas import UserIntent
+
+
+# def for the router agent
+
+async def router_agent(task:str) -> UserIntent:
+
+# istruction for the router
+    instructions = """ 
+    Sei un router in un workflow per calcolare lo stipendio netto 
+    a partire dalla RAL (Retribuzione Annua Lorda).
+
+    Classifica il messaggio dell'utente in una delle seguenti categorie:
+
+    - DATA: l'utente fornisce dati per avviare il calcolo.
+    Esempio: "Ho una ral di 35mila euro"
+
+    - UPDATE: l'utente vuole modificare dati di un calcolo già effettuato, qui devi valutare bene
+    Esempio: "E se la ral fosse 40mila?"
+
+    - INFO: l'utente chiede spiegazioni su temi legati al calcolo dello stipendio.
+    Esempio: "che cos'è IRPEF?"
+
+    - OFF_TOPIC: l'utente parla di argomenti non relativi a stipendio e tasse.
+    Esempio: "Chi è Donald Trump?"
+
+    Rispondi SOLO con il JSON richiesto.
+    """
+# build the router
+    router = Agent(name="router", instructions=instructions, model=oss_model, output_type=UserIntent)
+
+# run and trace the llm response
+    with trace("Check ral json"):  
+        result = await Runner.run(router, task)
+
+    return result.final_output
