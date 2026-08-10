ea876d1de443ab478666ee711407ae8f4a33b543
TDK-99 <vale.99.mezzo@gmail.com>
2026-08-10 02:27:33 +0200

    modify: insert the def of the agent of json builder

---
+++ src/src/json_builder_agent.py
@@ -0,0 +1,48 @@
+from agents import Agent, Runner, trace
+import json
+
+from config import  oss_model
+from schemas import RALInput
+
+
+
+# def for the router agent
+
+async def json_builder_agent(task:str) -> RALInput:
+
+# istruction for the router
+    instructions = """
+    Sei l'agente JSON Builder in un workflow per il calcolo dello stipendio netto 
+    a partire dalla RAL (Retribuzione Annua Lorda).
+
+    Il tuo unico obiettivo è raccogliere i dati dall'utente per completare il JSON di output.
+
+    Campi da compilare:
+    - ral: la Retribuzione Annua Lorda (obbligatorio, deve fornirlo l'utente)
+    - mensilita: 13 o 14 (default: 13)
+    - citta: città di residenza (default: Milano)
+    - figli_sotto_21: numero di figli sotto i 21 anni a carico (default: 0)
+    - figli_disabilita: numero di figli con disabilità a carico (default: 0)
+
+    Regole:
+    - Se l'utente fornisce solo la RAL, completa il JSON con i valori di default per gli altri campi.
+    - La ral puo essere scritta in vari 35k, 35 mila, il tuo obiettivo è trascriverlo in intero
+    - Se l'utente specifica valori diversi dai default, usa quelli.
+    - Se il messaggio è ambiguo o manca la RAL, chiedi chiarimenti in modo breve e diretto.
+    - Non inventare dati. Non fare calcoli. Il tuo lavoro è solo costruire il JSON.
+    - Rispondi SOLO con il JSON richiesto.
+    """
+# build the router
+    json_builder = Agent(name="json_builder", instructions=instructions, model=oss_model)
+
+# run and trace the llm response
+
+    with trace("Check ral json"):  # ← metti un nome tuo
+        result = await Runner.run(json_builder, task)
+
+        def parse_output(raw: str, model_class):
+            return model_class(**json.loads(raw))
+
+        parse_result= parse_output(result.final_output, RALInput)
+
+    return parse_result
